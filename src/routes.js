be613c91a8f830dc928008477caab0f1b354a891
Vikumsri <vchamindu@gmail.com>
2022-11-19 14:18:23 +0530

    C6 | IT19167992 | cleaning up | vikumsri

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 
 //handler apis
-const staffHandlerApi = require('./api/staff-handler.api')
 const messageHandlerApi = require('./api/message-handler.api')
 const fileHandlerApi = require('./api/file-handler.api')
 const userHandlerApi = require('./api/user-handler.api')
@@ -11,11 +10,8 @@ const authenticator = require('./authenticator/authenticator');
 
 
 module.exports =   function (app) {
+
     //API endpoints of user
-    app.use('/staff',staffHandlerApi());
-
-
-//API endpoints of user
     app.use('/user',userHandlerApi());
 
     //API endpoints of message
